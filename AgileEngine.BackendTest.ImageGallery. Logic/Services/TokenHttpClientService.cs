e0aa4824fa5a812ab80d558d1d5245ae6e960a06
icoria <ignacio.coria@external.iherb.com>
2021-02-28 14:20:32 -0300

    [DEV] HttpClient for Token Built

--- AgileEngine.BackendTest.ImageGallery. Logic/Services/TokenHttpClientService.cs
+++ AgileEngine.BackendTest.ImageGallery. Logic/Services/TokenHttpClientService.cs
@@ -2,16 +2,21 @@
 using System.Net.Http;
 using System.Net.Http.Headers;
 using System.Text;
-using System.Text.Json;
-using System.Text.Json.Serialization;
 using System.Threading.Tasks;
 using AgileEngine.BackendTest.ImageGallery.Logic.Models;
+using Microsoft.Extensions.Configuration;
 using Newtonsoft.Json;
 
 namespace AgileEngine.BackendTest.ImageGallery.Logic.Services
 {
     public class TokenHttpClientService
     {
+        public readonly IConfiguration _configuration;
+
+        public TokenHttpClientService(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
         public async Task<GalleryToken> PostToken(string apiKey)
         {
             try
@@ -19,9 +24,10 @@ namespace AgileEngine.BackendTest.ImageGallery.Logic.Services
                 using (var client = new HttpClient())
                 {
                     var Content = new StringContent(apiKey, Encoding.UTF8, "application/json");
+                    var baseUrl = new Uri(_configuration.GetSection("TokenInfo:BaseUrl").Value);
                     var url = "auth";
 
-                    client.BaseAddress = new Uri("https://api.github.com");
+                    client.BaseAddress = baseUrl;
                     client.DefaultRequestHeaders.Accept.Add(
                         new MediaTypeWithQualityHeaderValue("application/json"));
 
