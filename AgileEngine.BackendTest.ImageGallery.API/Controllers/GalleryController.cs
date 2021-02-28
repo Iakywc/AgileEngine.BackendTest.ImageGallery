ae019c34a99aa75befbe31f2def6d31874c71590
icoria <ignacio.coria@external.iherb.com>
2021-02-28 12:38:07 -0300

    [DEV] Initial Structured Developed

---
+++ AgileEngine.BackendTest.ImageGallery.API/Controllers/GalleryController.cs
@@ -0,0 +1,17 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AgileEngine.BackendTest.ImageGallery.API.Controllers
+{
+    public class GalleryController : ControllerBase
+    {
+        [HttpGet("Image")]
+        public async Task<ActionResult> GetImage()
+        {
+
+
+            return Ok();
+        }
+    }
+}
